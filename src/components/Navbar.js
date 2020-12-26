8643da4e4e707effb0b69b37c0fb063116b780bb
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-12-25 22:28:19 -0800

    Add style on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import "../styles/Navbar.css"
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -23,7 +24,7 @@ function Navbar() {
           <nav className="navbar">
             <div className="navbar-container">
               <Link to ="/" className="navbar-logo">
-                My Journey <i className="fa fa-fighter-jet" aria-hidden="true"></i>
+                My Journey <i className="fa fa-fighter-jet" aria-hidden="true" style={{paddingLeft: "20px"}}></i>
               </Link>   
              <div className= "menu-icon" onClick={handleClick}>
                <i className= {click ? "fas fa-times" : "fas fa-bars"} />
