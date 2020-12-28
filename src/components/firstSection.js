38ed612b0f26a3a66ae3745c694ee410eb0b83a4
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-12-28 13:28:20 -0800

    Created my first section

--- src/components/firstSection.js
+++ src/components/firstSection.js
@@ -1,13 +1,26 @@
 import React from "react";
 import video from "../videos/video-1.mp4";
+import {Button} from "../components/Button";
+import "../App.css";
+import "../styles/firstSection.css";
 
-function bodySection() {
+function firstSection() {
     return (
         <div className= "first-container">
             <video src={video} autoPlay loop muted/>
             <h1 >Adventure Awaits</h1>
+            <div className="first-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
+                Get Started
+                </Button>
+
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+                 Watch My Adventure <i className= "far fa-play-circle" />
+                </Button>
+
+            </div>
         </div>
     )
 }
 
-export default bodySection
+export default firstSection
