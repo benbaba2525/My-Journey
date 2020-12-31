53b5d851873124db8508b2316edbbfe1193f41ec
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-12-30 22:18:25 -0800

    Added cards items

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,11 +11,45 @@ function Cards() {
                 <ul className="cards__items">
                     <Carditem 
                     src="images/jamesbondIsland.jpg" 
-                    text="Explore my Adventure kytfitftcktckutfk8yogu;ohpn9uno"
-                    lable="Adventure"
+                    text="Travel through the James Bond Island 🌴 🌊 "
+                    label="Adventure"
                     path="/articles"
                     />
+
+                   <Carditem 
+                    src="images/ppIsland.jpg" 
+                    text="Explore my Adventure kytfitftcktckutfk8yogu;ohpn9unofc6rcu6rcuj uhbiu"
+                    label="Adventure"
+                    path="/articles"
+                    />   
                 </ul>
+
+                <ul className="cards__items">
+
+                   <Carditem 
+                    src="images/utah.jpg" 
+                    text="Explore my Adventure kytfitftcktckutfk8yogu;ohpn9unofc6rcu6rcuj uhbiu"
+                    label="Adventure"
+                    path="/articles"
+                    />   
+
+                    
+                   <Carditem 
+                    src="images/jashuatree.jpg" 
+                    text="Explore my Adventure kytfitftcktckutfk8yogu;ohpn9unofc6rcu6rcuj uhbiu"
+                    label="Adventure"
+                    path="/articles"
+                    />   
+
+                   <Carditem 
+                    src="images/diamondHead.jpg" 
+                    text="Explore my Adventure kytfitftcktckutfk8yogu;ohpn9unofc6rcu6rcuj uhbiu"
+                    label="Adventure"
+                    path="/articles"
+                    />   
+
+                </ul>
+
              </div>
             </div>
         </div>
