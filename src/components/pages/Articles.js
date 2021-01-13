ae668a8ebf320813e723bfcc5f2e41c494d77aa7
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2021-01-12 20:01:10 -0800

    Make it mobile responsive onn about page

--- src/components/pages/Articles.js
+++ src/components/pages/Articles.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../../App.css";
-//import Carditem from '../../components/CardItem';
 import "../../styles/Cards.css";
 import Footer from '../Footer';
+
 
 export default function Articles() {
     return (
@@ -14,33 +14,39 @@ export default function Articles() {
               <h1 className="aboutTrvelling">My Top 5 Favorite Places</h1>
               <p className="travelDesc"> 
               The world is filled with such a wide variety of wonderful destinations and beautiful places to visit, 
-              it can be quite difficult to compile a list of the best ones.
+              it can be quite difficult to compile a list of the best ones.I have my top 5 journey that would be an idea for you who cannot decide where to travel.
              </p>
             <br />
+            <hr />
              
 
              </div>
-             <div className="imgBox">
-                    
-          
-            <img src="images/diamondHead.jpg" className="imgAbout"></img>
-             
-                </div>
 
+               <div className="cards__wrapper">
+            
+                
+               <img src="images/diamondHead.jpg" className="imgAbout"></img>
+                <h2>Diamond Head, Hawii</h2>
+                <p className="travelDescSec">Sometimes travel can seem a distant thing, and we need a bit of travel inspiration to get us fired up.
+                   So this section aims to do exactly that. Giving you that travel inspiration you need to go out there and make travel happen.
+                 </p>
+          
+             </div>
                 
                 
                 <div className="imgBox">
-                <p className="travelDesc">Sometimes travel can seem a distant thing, and we need a bit of travel inspiration to get us fired up.
-                   So this section aims to do exactly that. Giving you that travel inspiration you need to go out there and make travel happen.
-             </p>
-            <br />
-             <p className="travelDesc">
-             I think travel makes everybody a more awesome person. We end our travels way better off than when we started. I’m not saying this to be conceited or egotistical; I’m saying it because I believe that travel is something that makes you not only a better human being but a way cooler one too. The kind of person people gravitate toward and want to be around.
-             </p>
+            
           
             <img src="images/utah2.jpg" className="imgAbout"></img>
+            <h2>Diamond Head, Hawii</h2>
+            <p className="travelDesc"> 
+              The world is filled with such a wide variety of wonderful destinations and beautiful places to visit, 
+              it can be quite difficult to compile a list of the best ones.I have my top 5 journey that would be an idea for you who cannot decide where to travel.
+             </p>
              
                 </div>
+
+                <hr />
 
                 <div className="imgBox">
                 <p className="travelDesc">Sometimes travel can seem a distant thing, and we need a bit of travel inspiration to get us fired up.
@@ -90,6 +96,7 @@ export default function Articles() {
     
 
     <Footer />
-    </div>)
+    </div>
+    )
     
 }
