b958b035064eea936bdabeafef3f66e627b9de59
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2021-01-15 00:24:20 -0800

    Added app.set to server.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
 const path = require('path');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
+app.set("port", PORT);
 
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port ,function(){
-  console.log("up and running on port "+port);
+
+app.listen(PORT ,function(){
+  console.log("up and running on port "+PORT);
 });
